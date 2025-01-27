79e30044a0d2df66e86a8ba225eb88a5a185470f
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-27 15:59:27 +0800

    route

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {port:5173}
-})
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
+// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   server: { port: 5173 },
-//   build: {
-//     rollupOptions: {
-//       external: ['react-toastify']
-//     }
-//   }
+//   server: {port:5173}
 // })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom','react-toastify'],
+    },
+  },
+});
