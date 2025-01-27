a8a76f8508c84392b1fc1caaeaa2e90408597e2c
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-27 16:48:46 +0800

    vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: { port: 5173 },
   build: {
     rollupOptions: {
-      external: ['react-toastify'] // Externalize specific modules
+      external: ['react-router-dom'] // Externalize specific modules
     }
   }
 })
