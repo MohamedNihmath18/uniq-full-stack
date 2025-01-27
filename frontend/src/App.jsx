0d20f34866e4e5b3689bbb456dc871fbc5db2d41
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-27 15:42:18 +0800

    now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import Verify from './pages/Verify';
 
 export const App = () => {
