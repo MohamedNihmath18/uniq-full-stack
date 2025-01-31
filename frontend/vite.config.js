67dcb5276ec7c374c1f3bb377e9fc30b7c59dda7
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-31 16:44:09 +0800

    config update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,17 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "react-toastify/dist/ReactToastify.css": "/node_modules/react-toastify/dist/ReactToastify.css"
-//     }
-//   },
-//   build: {
-//     rollupOptions: {
-//       external: ['react-router-dom', 'react-toastify'],
-//     },
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5174}
-})
+  resolve: {
+    alias: {
+      'react-toastify/dist/ReactToastify.css': path.resolve(__dirname, 'node_modules/react-toastify/dist/ReactToastify.css'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom', 'react-toastify'],
+    },
+  },
+});
