feca421ea477be388e0ea72e1eb83f8e76e1c6c4
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-31 16:30:22 +0800

    vercel update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "react-toastify/dist/ReactToastify.css": "/node_modules/react-toastify/dist/ReactToastify.css"
+    }
+  },
   build: {
     rollupOptions: {
       external: ['react-router-dom', 'react-toastify'],
