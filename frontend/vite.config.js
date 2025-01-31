f9a58af8472daf48d625ec25232344904f5e7190
Mohamed_Nihmathullah <mohamednihmath13@gmail.com>
2025-01-31 16:15:04 +0800

    vite chnages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { builtinModules } from 'module';
+
+const allExternal = [
+    ...builtinModules,
+    ...builtinModules.map((m) => `node:${m}`)
+]
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom', 'react-toastify'],
+      external: ['react-router-dom', 'react-toastify',...allExternal],
     },
   },
 });
